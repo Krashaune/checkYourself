1bf80e1dc0ea20ba7774d26b058987a1dd393cf5
Kiera <kiera.thomasson@gmail.com>
2018-07-02 14:37:10 -0700

    Initial Commit

---
+++ checkYourself/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  checkYourself
+//
+//  Created by kiera thomasson on 7/2/18.
+//  Copyright © 2018 kiera thomasson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

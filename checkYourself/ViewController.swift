931c19c692f7e745b17173ac20748a7a834a80c3
Kiera <kiera.thomasson@gmail.com>
2018-07-08 12:57:41 -0700

    Add custom login with spotify button to page. Remove all errors and reorganize login in view to account for programmatic button created.

--- checkYourself/ViewController.swift
+++ checkYourself/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SpotifyLogin
 
 
 var username = ""
@@ -16,17 +17,31 @@ class ViewController: UIViewController {
 
    
     @IBOutlet weak var loginOutlet: UITextField!
-    
+
     @IBAction func login(_ sender: UIButton) {
         if (loginOutlet.text != "") {
            username = loginOutlet.text!
             performSegue(withIdentifier: "segueLogin", sender: self)
         }
-        
+
     }
+    
+    @IBOutlet weak var spotifyLogin: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button = SpotifyLoginButton(viewController: self,
+                                        scopes: [.streaming,
+                                                 .userLibraryRead])
+        
+        self.view.addSubview(button)
+        self.spotifyLogin = button
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+//        spotifyLogin?.center = self.view.center
     }
 
     override func didReceiveMemoryWarning() {

cbe3c66e774cafdbaa7c5946050ffcfbffa143d5
Kiera <kiera.thomasson@gmail.com>
2018-07-24 14:07:05 -0700

    made formatting changes to the scroll view on the sulk view.

--- checkYourself/Slide.swift
+++ checkYourself/Slide.swift
@@ -11,8 +11,8 @@ import UIKit
 class Slide: UIView {
 
     @IBOutlet weak var imageView: UIImageView!
-    
-    @IBOutlet weak var textView: UITextView!
+
+    @IBOutlet weak var textLabel: UILabel!
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
--- checkYourself/ViewControllerSulk.swift
+++ checkYourself/ViewControllerSulk.swift
@@ -17,18 +17,34 @@ class ViewControllerSulk: UIViewController, UIScrollViewDelegate {
     func createSlides() -> [Slide] {
         
         let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
-        slide1.imageView.image = UIImage(named: "redSun")
-        slide1.textView.text = "A real-life bear"
+
+//        image view
+        let img1 = UIImage(named: "redSun")
+        slide1.imageView.image = img1
+//        slide1.imageView.alpha = 0.8
+
+//        text view
+//        let texttextLabel = slide1.texttextLabel(frame: CGRect(x: 0, y: 0, width: 250, height: 200))
+//        slide1.textLabel.backgroundColor = UIColor.lightGray
+//        slide1.textLabel.alpha = 1
+
+//        slide1.textLabel.font = UIFont.systemFont(ofSize: 24)
+//        slide1.textLabel.font = UIFont(name: "Papyrus", size: 24)
+//        UIColor(displayP3Red: 114, green: 109, blue: 109, alpha: 1)
         
+        slide1.textLabel.text = "Make a commitment that today you will not worry about anything. You will allow life to flow without expectation or judgement."
+        
+        
+    
         
         let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
         slide2.imageView.image = UIImage(named: "blueflowers")
-        slide2.textView.text = "A real-life bear"
+        slide2.textLabel.text = "A real-life bear"
         
         
         let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
         slide3.imageView.image = UIImage(named: "feather")
-        slide3.textView.text = "A real-life bear"
+        slide3.textLabel.text = "A real-life bear"
         
         
         return [slide1, slide2, slide3]
@@ -36,11 +52,11 @@ class ViewControllerSulk: UIViewController, UIScrollViewDelegate {
     
     func setupSlideScrollView(slides : [Slide]) {
         scrollView.frame = CGRect(x: 0, y: 40, width: view.frame.width, height: view.frame.height)
-        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: 600)
         scrollView.isPagingEnabled = true
         
         for i in 0 ..< slides.count {
-            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 30, width: view.frame.width, height: view.frame.height)
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
             scrollView.addSubview(slides[i])
         }
     }

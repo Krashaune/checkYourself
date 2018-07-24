ed012a1123b51e5c4a13b394be926863bf2adde9
Kiera <kiera.thomasson@gmail.com>
2018-07-23 23:42:02 -0700

    added a slide file and a xib view.

---
+++ checkYourself/Slide.swift
@@ -0,0 +1,21 @@
+//
+//  Slide.swift
+//  checkYourself
+//
+//  Created by kiera thomasson on 7/23/18.
+//  Copyright © 2018 kiera thomasson. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
